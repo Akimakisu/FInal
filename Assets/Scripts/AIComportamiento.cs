77254efa649ab9de91edfbf06377ecb594c897f7
Akimakisu <davidmesco@hotmail.com>
2017-11-09 14:22:11 -0500

    Initial commit

---
+++ Assets/Scripts/AIComportamiento.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class AIComportamiento : MonoBehaviour {
+
+	public float distanciaObjetivo;
+	public float distanciaSeguir;
+	public float velocidad;
+	public float orientacion;
+	private bool patrulla;
+	public Transform objetivo;
+	public Transform[] caminos;
+	private int camino;
+	private Rigidbody enemigo;
+
+
+	// Use this for initialization
+	void Start () {
+		enemigo = GetComponent<Rigidbody>();
+		patrulla = true;
+		camino = 0;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		distanciaObjetivo = Vector3.Distance (objetivo.position, transform.position);
+		if (distanciaObjetivo < distanciaSeguir) 
+		{
+			velocidad = 30;
+			SeguirObjetivo ();
+			MirarObjetivo ();
+			patrulla = false;
+		}
+		if ( patrulla== true ) 
+		{
+			Patrullar ();
+
+			if (distanciaObjetivo < distanciaSeguir) 
+			{
+				patrulla = false;
+
+			} 
+		}
+
+	}
+	void Patrullar()
+	{
+		if (Vector3.Distance (this.transform.position, caminos [camino].transform.position) > 1) 
+		{
+			Quaternion rotation = Quaternion.LookRotation (caminos[camino].position - transform.position);
+			transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * orientacion);
+			enemigo.AddForce(transform.forward * velocidad);
+		} 
+		else if (Vector3.Distance (this.transform.position, caminos [camino].transform.position) < 1) 
+		{
+ 			camino += 1;
+			if (camino > caminos.Length) 
+			{
+				camino = 0;
+
+			}
+		}
+	}
+	void MirarObjetivo()
+	{
+		Quaternion rotation = Quaternion.LookRotation (objetivo.position - transform.position);
+		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * orientacion);
+	}
+	void SeguirObjetivo()
+	{
+		enemigo.AddForce(transform.forward * velocidad);
+	}
+
+}
---
+++ Assets/Scripts/ControladorPersonaje.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControladorPersonaje : MonoBehaviour {
+
+	private Rigidbody jugador;
+	public float speed;
+	private int contador;
+	public Text llave;
+	public Text muerte;
+	public Text ganar;
+
+	void Start ()
+	{
+		jugador = GetComponent<Rigidbody> ();
+		contador = 0;
+		Contador ();
+		ganar.text = "";
+		muerte.text = "";
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		jugador.AddForce (movement * speed);
+
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("Llave"))
+		{
+		    Destroy(other.gameObject);
+			contador = contador + 1;
+			Contador ();
+		}
+		if (other.gameObject.CompareTag ("Enemigo")) 
+		{
+			muerte.text = "PERDISTE";
+			Time.timeScale = 0;
+		}
+	}
+
+	void Contador ()
+	{
+		llave.text = "Contador " + contador.ToString ();
+		if (contador >= 7) 
+		{
+			ganar.text = "GANASTE";
+		}
+	}
+}
